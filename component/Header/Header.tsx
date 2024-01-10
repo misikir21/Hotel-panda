9b41967fec447b51940c68ac685e6030555571e1
misikir21 <misikirkaka@gmail.com>
2024-01-10 11:14:04 +0300

    add colors and fontfaimily to tailwind configuration

--- component/Header/Header.tsx
+++ component/Header/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
       <div className="flex items-center w-full md:2/3">
-        <Link href="/" className="font-black text-[#f27405]">
+        <Link href="/" className="font-black text-tertiary-dark">
           HotelPanada
         </Link>
         <ul className="flex items-center ml-5">
