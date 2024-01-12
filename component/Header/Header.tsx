ba8fbe1198244e058ed358c720a53ab8a5b18606
misikir21 <misikirkaka@gmail.com>
2024-01-12 13:07:55 +0300

    update the darkmode icon to self-closing component

--- component/Header/Header.tsx
+++ component/Header/Header.tsx
@@ -7,13 +7,12 @@ function Header() {
   return (
     <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
       <div className="flex items-center w-full md:2/3">
-        <Link href="/" className="font-black text-[#f27405]">
-          HotelPanada
-        </Link>
+        <Link href="/" className="font-black text-[#f27405]" />
+        HotelPana
         <ul className="flex items-center ml-5">
           <li className="flex items-center">
             <Link href="/auth">
-              <FaUserCircle />
+              <FaUserCircle className="cursor-pointer" />
             </Link>
           </li>
           <li className="ml-2">
